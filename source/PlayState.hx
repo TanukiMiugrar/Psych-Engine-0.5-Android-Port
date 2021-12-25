3f0329204e5f2bcd2e718dddb9c8264481cf2465
TanukiMiugrar <96599613+TanukiMiugrar@users.noreply.github.com>
2021-12-25 16:35:48 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -68,15 +68,15 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
+		['Try tutorial', 0.2], //From 0% to 19%
+		['you stupid', 0.4], //From 20% to 39%
+		['7 y o', 0.5], //From 40% to 49%
+		['dumb', 0.6], //From 50% to 59%
+		['ok', 0.69], //From 60% to 68%
+		['v cool', 0.7], //69%
+		['yeah', 0.8], //From 70% to 79%
+		['YO', 0.9], //From 80% to 89%
+		['SICK!', 1], //From 90% to 99%
 		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
@@ -185,7 +185,7 @@ class PlayState extends MusicBeatState
 	public var camOther:FlxCamera;
 	public var cameraSpeed:Float = 1;
 
-	var dialogue:Array<String> = ['blah blah blah', 'coolswag'];
+	var dialogue:Array<String> = ['blah blah blah', 'dialog'];
 	var dialogueJson:DialogueFile = null;
 
 	var halloweenBG:BGSprite;
