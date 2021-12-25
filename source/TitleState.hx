6666ad6640cdfcf6961ade80e20cdaad49d3b70a
TanukiMiugrar <96599613+TanukiMiugrar@users.noreply.github.com>
2021-12-25 16:08:18 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -204,7 +204,7 @@ class TitleState extends MusicBeatState
 		if (titleJSON.backgroundSprite != null && titleJSON.backgroundSprite.length > 0 && titleJSON.backgroundSprite != "none"){
 			bg.loadGraphic(Paths.image(titleJSON.backgroundSprite));
 		}else{
-			bg.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+			bg.makeGraphic(FlxG.width, FlxG.height, FlxColor.YELLOW);
 		}
 			
 		add(bg);
